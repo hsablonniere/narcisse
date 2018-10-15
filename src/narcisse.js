1c638cccb7435c5713a62d200654b35d9ed964d5
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-10-15 18:58:02 +0200

    Update deps

--- src/narcisse.js
+++ src/narcisse.js
@@ -11,23 +11,27 @@ const url = require('url')
 const winston = require('winston')
 
 const pkg = require(path.resolve(projectRoot, 'package.json'))
+const logger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+})
 
 try {
   conf.validate({ allowed: 'strict' })
-  winston.info(`${pkg.name}@${pkg.version}`)
-  winston.info(`SEARCH_QUERIES:   ${conf.get('SEARCH_QUERIES').join(',')}`)
-  winston.info(`UNWANTED_DOMAINS: ${conf.get('UNWANTED_DOMAINS').join(',')}`)
-  winston.info(`DATABASE_PATH:    ${conf.get('DATABASE_PATH')}`)
+  logger.info(`${pkg.name}@${pkg.version}`)
+  logger.info(`SEARCH_QUERIES:   ${conf.get('SEARCH_QUERIES').join(',')}`)
+  logger.info(`UNWANTED_DOMAINS: ${conf.get('UNWANTED_DOMAINS').join(',')}`)
+  logger.info(`DATABASE_PATH:    ${conf.get('DATABASE_PATH')}`)
 }
 catch (error) {
   const configErrorMessages = error.message.split('\n')
   configErrorMessages.forEach((message) => {
-    winston.error('Bad config -', message)
+    logger.error(`Bad config - ${message}`)
   })
 
   // 9 - Invalid Argument
   // https://nodejs.org/api/process.html#process_exit_codes
-  winston.error('narcisse STOPPED because of configuration errors.')
+  logger.error('narcisse STOPPED because of configuration errors.')
   process.exit(9)
 }
 
@@ -37,7 +41,7 @@ const pusher = new PushBullet(conf.get('PUSH_BULLET_ACCESS_TOKEN'))
 
 function run () {
 
-  winston.info(`-- START: Let's find some links!`)
+  logger.info(`-- START: Let's find some links!`)
 
   const allSearches = conf.get('SEARCH_QUERIES')
     .map((query) => googleSearch(query))
@@ -52,25 +56,27 @@ function run () {
       })
       const newLinks = usefulLinks.filter((link) => linksDb.exists(link))
 
-      winston.info(`-- Found ${links.length} links on the Web`)
-      winston.info(`---- ${usefulLinks.length} interesting`)
-      winston.info(`---- ${newLinks.length} new`)
+      logger.info(`-- Found ${links.length} links on the Web`)
+      logger.info(`---- ${usefulLinks.length} interesting`)
+      logger.info(`---- ${newLinks.length} new`)
 
       newLinks.forEach((link) => {
 
-        winston.info(`.. adding link to DB    ${link.url}`)
+        logger.info(`.. adding link to DB    ${link.url}`)
         linksDb.add(link)
 
-        winston.info(`.. pushing notification ${link.url}`)
+        logger.info(`.. pushing notification ${link.url}`)
         const pushNoteText = `${link.text}\n${link.url}`
         pusher.note(null, `Narcisse [${link.searchQuery}]`, pushNoteText, (err) => {
-          if (err) winston.log(err)
+          if (err) {
+            logger.log(err)
+          }
         })
       })
 
-      winston.info(`-- DONE! There are ${linksDb.size()} links in the DB now`)
+      logger.info(`-- DONE! There are ${linksDb.size()} links in the DB now`)
     })
-    .catch((e) => winston.error(e))
+    .catch((e) => logger.error(e))
 }
 
 run()
